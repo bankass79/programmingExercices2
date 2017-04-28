700566f3e73934863dee92473ae68db27114846b
bankass79 <amadoumguindo@yahoo.fr>
2017-04-28 23:38:43 +0200

    test

---
+++ src/exercicesTest/StringMatchingTest.java
@@ -0,0 +1,21 @@
+package exercicesTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import exercices.StringMatching;
+
+public class StringMatchingTest {
+	
+	
+	@Test
+	public void testName() throws Exception {
+		
+		StringMatching string = new StringMatching();
+		
+		assertEquals(string, string);
+		
+	}
+
+}
